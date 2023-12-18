4136191b9702e84e0b9fd015bdf5f627ad7f56f1
Вяселева Динара <118040854+VyaselevaDinara@users.noreply.github.com>
2023-12-18 20:22:00 +0300

    Update bloknot.cs

--- bloknot.cs
+++ bloknot.cs
@@ -1,4 +1,4 @@
-﻿using System.Diagnostics;
+﻿using System.Diagnostics; // Код использует пространства имен System.Diagnostics и System.Timers
 using System.Timers;
 
 
@@ -8,14 +8,14 @@ internal class Program
     public static int counter = 0;
     private static void Main(string[] args)
     {
-        System.Timers.Timer timer = new(2000);
-        timer.Elapsed += StartProcess;
+        System.Timers.Timer timer = new(2000);  // Создание таймера с интервалом 2000 миллисекунд (2 секунды)
+        timer.Elapsed += StartProcess; // Добавление обработчиков событий на срабатывание таймера
         timer.Elapsed += OnTimedEvent;
-        timer.Start();
+        timer.Start();   // Запуск таймера
         bool flag = true;
         while (flag)
         {
-            if (counter == 10)
+            if (counter == 10) // Если счетчик достигает 10, останавливаем таймер
             {
                 flag = false;
                 timer.Stop();
@@ -25,21 +25,23 @@ internal class Program
 
     private static void StartProcess(object source, ElapsedEventArgs e)
     {
-        Process myProc = null;
+        Process myProc = null; 
 
         try
         {
-            myProc = Process.Start("Notepad.exe");
+            myProc = Process.Start("Notepad.exe"); // Запуск процесса "notepad.exe" (блокирующий вызов)
         }
         catch (Exception ex)
         {
             Console.WriteLine(ex);
         }
-        counter++;
+        counter++;  // Увеличение счетчика
     }
     private static void OnTimedEvent(object source, ElapsedEventArgs e)
     {
-        Console.WriteLine("Приложение было запущено в {0:HH:mm:ss.fff}",
+        Console.WriteLine("Приложение было запущено в {0:HH:mm:ss.fff}", // Вывод времени срабатывания таймера в консоль
                           e.SignalTime);
     }
 }
+// В данном коде создается таймер, который каждые 2 секунды запускает процесс "notepad.exe",
+// при этом выводя в консоль время запуска. Таймер останавливается после 10 запусков процесса.
